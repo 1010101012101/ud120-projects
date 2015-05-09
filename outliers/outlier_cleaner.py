0abd33524e4121bf3998fc0e23975ae63e42c15e
ChristosCharisiadis <chcharisi@gmail.com>
2015-05-09 13:29:16 +0300

    Lesson 7 code finished.

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,10 @@ def outlierCleaner(predictions, ages, net_worths):
         return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error)
     """
-    
-    cleaned_data = []
-
-    ### your code goes here
-
-    
+    errors = [(predictions[i] - net_worths[i]) ** 2 for i in range(len(ages))]
+    zipped = zip(errors, ages, net_worths)
+    zipped.sort()
+    zipped = zipped[:len(zipped) - len(zipped)/10]
+    cleaned_data = [(age, net, error) for (error, age, net) in zipped]
     return cleaned_data
 
--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -25,10 +25,11 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
-
-
-
-
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit (ages_train, net_worths_train)
+print ('slope and intercept: ', reg.coef_, reg.intercept_)
+print ('test set score: ' + str(reg.score(ages_test, net_worths_test)))
 
 
 
@@ -77,6 +78,8 @@ if len(cleaned_data) > 0:
     plt.xlabel("ages")
     plt.ylabel("net worths")
     plt.show()
+    print ('new slope and intercept: ', reg.coef_, reg.intercept_)
+    print ('new test set score: ' + str(reg.score(ages_test, net_worths_test)))
 
 
 else:
