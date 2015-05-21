319ad1ef5f70f21738ca44c3ed9eeff1499b9a59
ChristosCharisiadis <chcharisi@gmail.com>
2015-05-21 18:35:56 +0300

    Lesson 14 code finished.

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -30,3 +30,27 @@ labels, features = targetFeatureSplit(data)
 ### your code goes here 
 
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.cross_validation import train_test_split
+from numpy import zeros
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print (accuracy_score(labels_test, pred))
+print ('POI number: ' + str(pred.sum()))
+print ('Total test people: ' + str(pred.size))
+all_zero_pred = zeros(29)
+print (accuracy_score(labels_test, all_zero_pred))
+print ('Precesion: ' + str(precision_score(labels_test, pred)))
+print ('Recall: ' + str(recall_score(labels_test, pred)))
+
+predictions = [0, 1, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1]
+true_labels = [0, 0, 0, 0, 0, 0, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 0]
+print ('Precesion: ' + str(precision_score(true_labels, predictions)))
+print ('Recall: ' + str(recall_score(true_labels, predictions)))
